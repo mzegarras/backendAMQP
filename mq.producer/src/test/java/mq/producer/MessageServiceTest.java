20970e086c2b0b18e31e463c1895d6d9a37f9d0b
manuelzegarra <mzegarra@gmail.com>
2015-12-05 08:35:28 -0500

    Unit test example

--- mq.producer/src/test/java/mq/producer/MessageServiceTest.java
+++ mq.producer/src/test/java/mq/producer/MessageServiceTest.java
@@ -1,6 +1,7 @@
 package mq.producer;
 
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertNotNull;
 
 import java.util.UUID;
 
@@ -10,11 +11,13 @@ import javax.jms.Queue;
 import javax.jms.Session;
 import javax.jms.TextMessage;
 
+import org.apache.log4j.BasicConfigurator;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.jms.core.MessageCreator;
 import org.springframework.test.context.ContextConfiguration;
@@ -24,22 +27,31 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {
-		"file:src/test/resources/spring/applicationContextTest.xml"})
-@Ignore
+		"file:src/test/resources/spring/applicationContextTest0.xml"})
 public class MessageServiceTest {
 
 	
 	@Autowired
 	JmsTemplate jmsTemplate;
 	
+	@Value("${queue}")
+    public String queue;
+
+	
+	
 	/**
 	 * Test setup.
 	 */
 	@Before
 	public void setUp() {
-		//message = "TestSpringJMSMQ test message.";
+		 BasicConfigurator.configure();
 	}
 	
+	
+	@Test
+	public void jsmTemplateNull(){
+		assertNotNull(jmsTemplate);
+	}
 	
 	@Test
 	public void testSendMessage() {
